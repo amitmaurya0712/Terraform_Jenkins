a41c3a8cc5beff76f0b62877e88cd76a66c70e3d
Amit Maurya <xs299-amimau@xs-host603-056.xs.neuralcompany.team>
2023-05-01 16:33:49 +0530

    files added

--- rds.tf
+++ rds.tf
@@ -5,8 +5,8 @@ resource "aws_db_parameter_group" "default" {
 resource "aws_db_instance" "default" {
   allocated_storage           = 10
   db_name                     = "SimpleJenkinsDB"
-  # engine                      = "MySQL"
-  # engine_version              = "8.0.32"
+  engine                      = "MySQL"
+  engine_version              = "8.0.32"
   instance_class              = "db.t3.micro"
   manage_master_user_password = true
   username                    = "admin"
