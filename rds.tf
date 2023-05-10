ef568e58fd72f38db1d9d3642c7f0335f812b34a
amitmaurya0712 <130205033+amitmaurya0712@users.noreply.github.com>
2023-05-10 12:36:14 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,15 +1,4 @@
-
-
-resource "aws_key_pair" "TF_key" {
-
-  key_name   = "TF_key"
-
-  public_key = tls_private_key.rsa.public_key_openssh
-
-}
-
  
-
 resource "tls_private_key" "rsa" {
 
   algorithm = "RSA"
